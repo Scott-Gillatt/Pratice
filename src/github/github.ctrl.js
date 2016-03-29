c70d1ef5b3ecfaccb98a4cd8b93d3549fc66b16f
Scott Gillatt <scottgillatt@gmail.com>
2016-03-28 20:22:41 -0600

    we are module

--- src/github/github.ctrl.js
+++ src/github/github.ctrl.js
@@ -1,4 +1,5 @@
-app.controller("GitHubController", function($scope, GitHubService){
+angular.module("GitHub")
+.controller("GitHubController", function($scope, GitHubService){
     GitHubService.fetchUsers().success(function (users){
         $scope.users = users
     })
--- src/github/github.svc.js
+++ src/github/github.svc.js
@@ -1,4 +1,5 @@
-app.factory("GitHubService", function ($http){
+angular.module("GitHub")
+.factory("GitHubService", function ($http){
     return {
         fetchUsers: function (){
             return $http.get("https://api.github.com/users?since=16550260")
