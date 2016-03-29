308a76c097218ecb080a69dbe9fd3c01e8eeeef8
Scott Gillatt <scottgillatt@gmail.com>
2016-03-28 20:11:01 -0600

    Seperate files out into folders

--- src/module.js
+++ src/module.js
@@ -1,15 +1,1 @@
 var app = angular.module("GitHub", [])
-
-app.factory("GitHubService", function ($http){
-    return {
-        fetchUsers: function (){
-            return $http.get("https://api.github.com/users?since=16550260")
-        }
-    }
-})
-
-app.controller("GitHubController", function($scope, GitHubService){
-    GitHubService.fetchUsers().success(function (users){
-        $scope.users = users
-    })
-})
